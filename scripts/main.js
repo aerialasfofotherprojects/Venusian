45edbe22a6fcdc935a5fd4a4623b3bac4526a88d
books4thoughtgit <avelinaenriquez1956@gmail.com>
2024-03-17 19:33:22 +0800

    Im losing my mind

--- scripts/main.js
+++ scripts/main.js
@@ -2,3 +2,9 @@ require('blocks');
 require('units');
 require('planets');
 require('items');
+
+Events.on(ClientLoadEvent, e => {
+    const dialog = new BaseDialog("This mod is in W.I.P. Do you wish to continue?");
+    dialog.cont.button("Yes", () => dialog.hide());
+    dialog.cont.button("No", () => Core.app.exit());
+});
