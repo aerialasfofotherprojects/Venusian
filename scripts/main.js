ec699ac3d387ad9b161217b0a0414dd760b3c6de
books4thoughtgit <avelinaenriquez1956@gmail.com>
2024-03-21 17:01:30 +0800

    update

--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,21 @@
 const venus = Vars.content.planet("venusian-venus");
 
-function loadContent() { //running this func when yes is pressed
-    
-    //venus configs
-    venus.ruleSetter = r => {
-        //for rules, check
-        //https://github.com/Anuken/Mindustry/core/src/mindustry/game/Rules.java
-        r.waveTeam = Team.crux
-        r.placeRangeCheck = false
-        r.showSpawns = true
-        r.fog = true
-        r.staticFog = true
-        r.lighting = false
-        r.coreDestroyClear = true
-    }
-    //planet default attributes
-    venus.defaultAttributes.set(Attributes.heat, 0.8);
-    //blocks and items unlocked in tech tree when first playing the planet
-    venus.unlockedOnLand.add("core-ember");
+//venus configs
+venus.ruleSetter = r => {
+    //for rules, check
+    //https://github.com/Anuken/Mindustry/core/src/mindustry/game/Rules.java
+    r.waveTeam = Team.crux
+    r.placeRangeCheck = false
+    r.showSpawns = true
+    r.fog = true
+    r.staticFog = true
+    r.lighting = false
+    r.coreDestroyClear = true
 }
-
-loadContent();
+//planet default attributes
+venus.defaultAttributes.set(Attributes.heat, 0.8);
+//blocks and items unlocked in tech tree when first playing the planet
+venus.unlockedOnLand.add("core-ember");
 
 Events.on(ClientLoadEvent, e => {
     const dialog = new BaseDialog("Warning! W.I.P. Mod!");
