fbf337c1c24acca2a4d1b308b7fd053da3e02e05
books4thoughtgit <avelinaenriquez1956@gmail.com>
2024-03-21 18:34:05 +0800

    update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,5 @@
 const venus = Vars.content.planet("venusian-venus");
 
-//venus configs
-//blocks and items unlocked in tech tree when first playing the planet
-venus.unlockedOnLand.add("core-ember");
-
 Events.on(ClientLoadEvent, e => {
     const dialog = new BaseDialog("Warning! W.I.P. Mod!");
     dialog.cont.add("This mod is in [red]W.I.P.[]! Do you [yellow]wish[] to continue?")
