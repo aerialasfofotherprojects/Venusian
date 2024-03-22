5a8176f4f8052c0f68683f4eeb4f90dee643a2eb
books4thoughtgit <avelinaenriquez1956@gmail.com>
2024-03-22 16:13:13 +0800

    update

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,6 @@
-const venus = Vars.content.planet("venusian-venus");
-
-if (venus != null) {
-    venus.defaultAttributes.set(Attribute.heat, 0.8)
-}
-
 Events.on(ClientLoadEvent, e => {
     const dialog = new BaseDialog("Warning! W.I.P. Mod!");
-    dialog.cont.add("This mod is in [red]W.I.P.[]! Do you [yellow]wish[] to continue?")
+    dialog.cont.add("This mod is in [red]W.I.P.[]! Do you [yellow]wish[] to continue?").row();
     dialog.cont.button("Yes", () => {
         dialog.hide();
     }).row();
