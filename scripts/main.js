1ab6a052e8e2e66e98ba721daf6bfa57f23595b8
books4thoughtgit <avelinaenriquez1956@gmail.com>
2024-03-23 09:55:45 +0800

    trying to fix

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,7 @@
 Events.on(ClientLoadEvent, e => {
     const dialog = new BaseDialog("Warning! W.I.P. Mod!");
-    dialog.cont.add("This mod is in [red]W.I.P.[]! Do you [yellow]wish[] to continue?").row();
-    dialog.cont.button("Yes", () => {
-        dialog.hide();
-    });
+    dialog.cont.add("This mod is in [red]W.I.P[]! Do you [yellow]wish[] to continue?").row();
+    dialog.cont.button("Yes", () => dialog.hide());
     dialog.cont.button("No", () => Core.app.exit());
     dialog.show();
 });
