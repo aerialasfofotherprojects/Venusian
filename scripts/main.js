d8f2a1d2d4ecbd9048d5f44782545a6af31c1bbb
books4thoughtgit <avelinaenriquez1956@gmail.com>
2024-04-05 19:36:28 +0800

    demo preparation

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 Events.on(ClientLoadEvent, e => {
     const dialog = new BaseDialog("Warning! W.I.P. Mod!");
     dialog.cont.add("This mod is in [red]W.I.P[]! Do you [yellow]wish[] to continue?").row();
+    dialog.cont.add("Also this is a [blue]demo[] version so feel free to press yes and try it out!").row();
     dialog.cont.button("Yes", () => dialog.hide());
     dialog.cont.button("No", () => Core.app.exit());
     dialog.show();
